7c8461e36e4a6f799284401cc6cb0b1104f64465
cb23zr <105169646+cb23zr@users.noreply.github.com>
2023-06-28 22:32:37 +0200

    Add files via upload

---
+++ src/app/pages/registration/registration.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {Location} from "@angular/common";
+import {AuthService} from "../../shared/services/auth.service";
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.scss']
+})
+export class RegistrationComponent {
+  signupForm = new FormGroup({
+    uname: new FormControl(''),
+    psw: new FormControl(''),
+    repsw: new FormControl(''),
+    username: new FormControl(''),
+    name: new FormGroup({
+      fname: new FormControl(''),
+      lname: new FormControl(''),
+    }),
+  });
+
+  constructor(private location: Location, private authService:AuthService) {
+  }
+
+  OnSubmit(){
+    this.authService.signup(this.signupForm.get('uname')?.value, this.signupForm.get('psw')?.value).then(cred => {
+      console.log(cred);
+    }).catch(error => {
+      console.error(error);
+    });
+  }
+
+  Back(){
+    this.location.back();
+  }
+
+}
